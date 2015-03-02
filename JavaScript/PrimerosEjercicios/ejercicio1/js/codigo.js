b3c52d4df15947f92344342604a2824c94fb3ffd
Itziar Olazabal Alonso <ItziarOlazabalAlonso@iMac-de-Itziar.local>
2015-03-02 17:21:36 +0100

    EjercicioJavaScript

---
+++ JavaScript/PrimerosEjercicios/ejercicio1/js/codigo.js
@@ -0,0 +1,3 @@
+//Se muestra en la consola lo que le indicas
+console.log("Hola Mundo!");
+console.log("Soy el primer script");
---
+++ JavaScript/PrimerosEjercicios/ejercicio3/js/codigo.js
@@ -0,0 +1,7 @@
+var mes = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+for(i in mes) {
+
+console.log(mes[i]);
+
+}
