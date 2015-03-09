d9e9a25119edd676307f0f31dd76cdb767f81e7e
unknown <FPE@965PDS3-006.aeg8.edu>
2015-03-09 14:15:09 +0100

    Ejercicio JavaScript_formulario

---
+++ JavaScript/ejercicio-Formulario/js/codigo.js
@@ -0,0 +1,34 @@
+window.onload(function(
+
+
+
+    function validar(event){
+
+            var nombre = document.getElementById("#resgistro_nombre");
+            var apellido = document.getElementById("#resgistro_apellido");
+            var email = document.getElementById("#registro_email");
+            var contrasena = document.getElementById("#registro_password");
+            var comentario = document.getElementById("#registro_comentarios");
+
+            if(nombre == "" || apellido == "" || email == ""){
+
+                    console.log("Complete los campos obligatorios");
+
+            }
+            else{
+                var mail = /^(\w+)((\.|-|_)(\w+))*@(\w+)(\.\w{2,})+$/;
+
+                if(email != mail){
+                    console.log("escriba un email válido");
+                }
+
+            }
+
+            if(contrasena>6){
+                console.log("La contraseña debe tener como mínimo 6 caracteres");
+            }
+
+    }
+              //Llamar a la function validar
+
+ ));
